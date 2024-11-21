0520e8dc6678692c7633d894f6d6c0c7a7acc853
Br14n-M4rt <23030256@itesa.edu.mx>
2024-11-21 10:52:14 -0600

    Version01.Listas

---
+++ Enumerable.UI.Consola/Alumnos.cs
@@ -0,0 +1,14 @@
+﻿namespace Enumerable.UI.Consola
+{
+    public class Alumnos
+    {
+        public int Matricula { get; init; }
+        public string ?Nombre { get; init; }
+        public decimal Precio { get; init; }
+
+        public override string ToString()
+        {
+            return $"{Matricula}\t{Nombre}\t{Precio:C2}";
+        }
+    }
+}
---
+++ Enumerable.UI.Consola/Program.cs
@@ -0,0 +1,38 @@
+﻿using Enumerable.UI.Consola;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("====================");
+        Console.WriteLine("Enumerable");
+        Console.WriteLine("====================");
+        Console.WriteLine("\n");
+        Console.WriteLine("**Lista**  list<T>");
+        #region Lista
+        var lista = new List<Alumnos>
+        {
+            new Alumnos {Matricula = 1, Nombre = "Juan", Precio = 24.459M},
+            new Alumnos {Matricula = 2, Nombre = "José", Precio = 22.990M},
+            new Alumnos {Matricula = 3, Nombre = "Luis", Precio = 18.980M},
+            new Alumnos {Matricula = 4, Nombre = "Ileny", Precio = 17.345M},
+            new Alumnos {Matricula = 5, Nombre = "Karla", Precio = 20.234M},
+            new Alumnos {Matricula = 6, Nombre = "Casco", Precio = 19.675M},
+            new Alumnos {Matricula = 7, Nombre = "Mitzi", Precio = 21.999M},
+            new Alumnos {Matricula = 8, Nombre = "Isabel", Precio = 15.342M},
+            new Alumnos {Matricula = 9, Nombre = "Zury", Precio = 14.999M}
+
+        };
+
+        lista = lista.OrderBy(x => x.Precio).ToList();
+
+        for (int i = 0; i < lista.Count; i++)
+        {
+            Console.WriteLine(lista[i]);
+        }
+
+        #endregion
+
+        Console.ReadKey();
+    }
+}
